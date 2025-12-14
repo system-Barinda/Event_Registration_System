1ac306cac105b20806c149f32fdb1f3dd7c0633f
system sylvere BARINDA <systembarinda@gmail.com>
2025-12-15 00:21:39 +0200

    hosted and README file initialization commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Event_Registration_System
 Event Registration System
+
+# GROUP DEVELOPERS
+
+```bash
+1: System BARINDA CO.DEVELOPER
+2: Kellia KAYITARE SE.DEVELOPER
+
+```
+
+## HOW TO RUN OUR PROJECT
+
+```bash
+Our project has live link https://eventregsandk.wasmer.app/user/my_events.php
+and the credentials to test our system are:
+username: systembarinda@gmail.com
+password: 123
+Thank you.
+
+```
+
+![Screenshot](image-1.png)
--- config/db.php
+++ config/db.php
@@ -1,12 +1,12 @@
 <?php
-$host = "localhost";
-$dbname = "barinda_library_management";
-$username = "root";
-$password = "";
+$host = "db.fr-pari1.bengt.wasmernet.com";
+$dbname = "dbLPmdYroL9h4AHd4XY2D8CW";
+$username = "f358599c75ae8000ff78c60d765d";
+$password = "0693f358-599d-7320-8000-84dfb959a00c";
 
 try {
     $pdo = new PDO(
-        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        "mysql:host=$host;dbname=$dbname;charset=utf8; port=10272",
         $username,
         $password
     );
